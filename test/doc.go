6fa5b9f3875e9f7e697bf5cdff24d6d19ce6fa01
Kevin Burke <kev@inburke.com>
2017-11-27 20:39:32 -0800

    all: fix errors reported by vet, golint
    None are "wrong" per se, but there are a lot of good suggestions and
    in one case a docstring that was not present in godoc due to the
    presence of an extra newline.
    
    Changed "Id" in struct properties to "ID" in some non-exported
    structs. Removed a trailing period from some error messages; I believe
    the exact contents of error strings are not covered by the Go
    compatibility promise.
    
    Change-Id: I7c620582dc247396f72c52d38c909ccc0ec87b83
    Reviewed-on: https://go-review.googlesource.com/80145
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- test/doc.go
+++ test/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This package contains integration tests for the
+// Package test contains integration tests for the
 // github.com/bored-engineer/ssh package.
 package test // import "github.com/bored-engineer/ssh/test"
